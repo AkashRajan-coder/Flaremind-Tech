5302327fed8b257755a0a28724f0b1d0bde74d69
Akash <rajanakash331@gmail.com>
2025-10-03 14:29:57 +0530

    Update transporter.js

--- controller/transporter.js
+++ controller/transporter.js
@@ -6,7 +6,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // true if port 465
+  secure: true, // true for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
