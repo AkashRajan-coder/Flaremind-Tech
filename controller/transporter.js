0cbcea4787ff5d6c07f7bbbd02b9ccc18c473f61
Akash <rajanakash331@gmail.com>
2025-10-04 13:20:11 +0530

    Update transporter.js

--- controller/transporter.js
+++ controller/transporter.js
@@ -1,22 +1,22 @@
-// import nodemailer from "nodemailer";
-// import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
-// dotenv.config(); // Load .env
+dotenv.config(); // Load .env
 
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS, // App Password
-//   },
-//   logger: true,
-//   debug: true,
-// });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS, // App Password
+  },
+  logger: true,
+  debug: true,
+});
 
-// // Verify connection
-// transporter.verify()
-//   .then(() => console.log("Mail server ready"))
-//   .catch(console.error);
+// Verify connection
+transporter.verify()
+  .then(() => console.log("Mail server ready"))
+  .catch(console.error);
 
 // export default transporter; // ES module default export
 // import nodemailer from "nodemailer";
@@ -46,34 +46,8 @@
 //   .then(() => console.log("✅ Gmail SMTP ready on Render"))
 //   .catch((err) => console.error("❌ SMTP connection error:", err));
 
-// export default transporter;
+export default transporter;
 
-// mailer.js
-import { Resend } from "resend";
-import dotenv from "dotenv";
-dotenv.config();
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export const sendMail = async (mailOptions) => {
-  try {
-    const { to, subject, html, text } = mailOptions;
-
-    const { data, error } = await resend.emails.send({
-      from: process.env.DEFAULT_FROM,
-      to,
-      subject,
-      html: html || `<p>${text}</p>`,
-    });
-
-    if (error) throw error;
-    console.log(`✅ Email sent to ${to}`);
-    return { success: true };
-  } catch (err) {
-    console.error(`❌ Failed to send email to ${mailOptions.to}:`, err.message);
-    return { success: false, error: err.message };
-  }
-};
 
 
 // xsmtpsib-7f34199e3abd46b235ec37e1f53f86be2033f7b5379ee16cfa17eded75c9c0ec-h0cEpnSJVkNRajbz
